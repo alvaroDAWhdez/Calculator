95c9e33ef2f89b067b6ac2ebefad09979a2b5f1b
alvaroDAWhdez <2907905@alu.murciaeduca.es>
2024-12-18 13:07:24 +0100

    Creado main y operadores

---
+++ calculadora.java
@@ -0,0 +1,8 @@
+public class calculadora {
+    public static void main(String[] args) {
+    // Valores predefinidos (puedes cambiarlos aquí)
+    double num1 = 10.5;
+    double num2 = 3.2;
+   
+    }
+}
