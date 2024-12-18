a4d5b0dfc12062c656c56d21e1e83e3bbbd4f59a
alvaroDAWhdez <2907905@alu.murciaeduca.es>
2024-12-18 13:09:00 +0100

    Creado operacion suma

--- calculadora.java
+++ calculadora.java
@@ -3,6 +3,17 @@ public class calculadora {
     // Valores predefinidos (puedes cambiarlos aquí)
     double num1 = 10.5;
     double num2 = 3.2;
+    char operador = '+';
+    double resultado = 0;
+    switch (operador) {
+        case '+':
+            resultado = num1 + num2;
+            break;
+        default:
+            System.out.println("Operador inválido");
+        return;
+    }
+    System.out.println("Resultado: " + resultado);
    
     }
 }
